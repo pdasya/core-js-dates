be41c11d8262bea145adf88b887b0bc694b68c9b
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-03-13 11:18:02 +0900

    feat: solve getDayName task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -50,8 +50,19 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(inputDate) {
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const date = new Date(inputDate);
+  const dayIndex = date.getUTCDay();
+  return days[dayIndex];
 }
 
 /**
