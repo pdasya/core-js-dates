7ec730cd08d082713a7807a6c84acc94f31d8289
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-03-13 11:46:49 +0900

    feat: solve isDateInPeriod task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -134,8 +134,12 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const checkDate = new Date(date);
+  const startPeriodDate = new Date(period.start);
+  const endPeriodDate = new Date(period.end);
+
+  return checkDate >= startPeriodDate && checkDate <= endPeriodDate;
 }
 
 /**
